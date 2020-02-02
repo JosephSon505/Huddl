896a961c82d5bf5d7cce4af6cae644e0b66631b1
Joseph Son <son.joseph98@gmail.com>
2020-02-02 11:47:54 -0800

    update gitignore and home.js

--- Huddl-client/src/pages/home.js
+++ Huddl-client/src/pages/home.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
+
+// Material-UI Imports
+import withStyles from '@material-ui/core/styles/withStyles';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  borderRight: {
+    borderRight: '1px solid black',
+    backgroundColor: '#1EA37D',
+    height: '100%'
+  }, 
+  card: {
+    backgroundColor: '#1EA37D',
+  },
+  container: {
+    height: '100%'
+  }
+};
 
 class home extends Component {
   render() {
+    const { classes } = this.props;
+
     return (
-      <div className='container'>
+      <div className={ classes.container }>
         <Grid container spacing={10}>
-          <Grid item sm={2} xs={12}>
-            <p>Users / Groups</p>
+          <Grid item sm={4} xs={12} className={classes.borderRight}>
+            <Typography variant='h4'>
+              Profile Section
+            </Typography>
+
+            <Typography variant='h4'>
+              Forums
+            </Typography>
+
+            <Typography variant='h4'>
+              Direct Messages
+            </Typography>
+
+
+            <Typography variant='h4'>
+              Telepsychiatry
+            </Typography>
+            
           </Grid>
-          <Grid item sm={10} xs={12}>
+
+          <Grid item sm={8} xs={12}>
             <p>Messaging platform</p>
           </Grid>
         </Grid>
@@ -18,4 +55,4 @@ class home extends Component {
   }
 }
 
-export default home;
+export default withStyles(styles)(home);
