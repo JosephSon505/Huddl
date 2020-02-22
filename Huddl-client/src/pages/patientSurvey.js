fa856fa2d187fbc514b0c55fb06aa427eea7d438
natekong <yichunko@usc.edu>
2020-02-22 05:41:25 -0800

    finished survey

--- Huddl-client/src/pages/patientSurvey.js
+++ Huddl-client/src/pages/patientSurvey.js
@@ -3,34 +3,10 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import Radio from '@material-ui/core/Radio';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-import Button from '@material-ui/core/Button';
+import PatientForm from '../components/PatientForm';
 
 import React, { Component } from 'react';
-
-const StyledRadio = withStyles({
-    root: {
-        color: '#000000',
-        '&$checked': {
-            color: '#000000',
-        },
-    },
-    checked: {},
-})(props => <Radio color="default" {...props} />);
-
-const Chatkit = require('@pusher/chatkit-server');
-const instanceLocator = 'v1:us1:3366eda2-e4d8-45c1-9f80-00f24eb6f202';
-const key = '1f7ad742-25f6-4c95-ab73-740747059cb3:yD1HqPGZMZg1yZrWAK36oQvEiBckt54dNrmoJahyoLc=';
-
-const chatkit = new Chatkit.default({
-    instanceLocator: instanceLocator,
-    key: key,
-})
-
+import axios from 'axios';
 
 const styles = {
     container: {
@@ -38,62 +14,42 @@ const styles = {
     },
 };
 
-const useStyles = makeStyles(theme => ({
-    formControl: {
-        margin: theme.spacing(3),
-    },
-}));
-
-function RadioGroupOne() {
-    const classes = useStyles();
-    const [value, setValue] = React.useState('female');
-
-    const handleChange = event => {
-        setValue(event.target.value);
-    };
-
-    return (
-        <div>
-            <FormControl component="fieldset" className={classes.formControl}>
-                <FormLabel component="legend">Do you have a preference for your provider?</FormLabel>
-                <RadioGroup aria-label="gender" name="gender" value={value} onChange={handleChange}>
-                    <FormControlLabel value="female" control={<StyledRadio />} label="Female provider" />
-                    <FormControlLabel value="male" control={<StyledRadio />} label="Male provider" />
-                    <FormControlLabel value="other" control={<StyledRadio />} label="No preference" />
-                </RadioGroup>
-            </FormControl>
-        </div>
-
-    );
-}
-
-function RadioGroupTwo() {
-    const classes = useStyles();
-    const [value, setValue] = React.useState('therapy');
-
-    const handleChange = event => {
-        setValue(event.target.value);
-    };
-
-    return (
-        <div>
-            <FormControl component="fieldset" className={classes.formControl}>
-                <FormLabel component="legend">What are you looking to get out of this service?</FormLabel>
-                <RadioGroup aria-label="type" name="type" value={value} onChange={handleChange}>
-                    <FormControlLabel value="therapy" control={<StyledRadio />} label="More general, feeling stressed, want someone to talk to" />
-                    <FormControlLabel value="support" control={<StyledRadio />} label="Specialised trauma support" />
-                </RadioGroup>
-            </FormControl>
-        </div>
-
-    );
-}
-
 class patientSurvey extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            loading: false,
+            errors: {
+                "gender": "",
+                "type": "",
+            }
+        }
+    }
+
+    handleSurvey = (surveyData) => {
+        this.setState({
+            loading: true,
+            errors: {
+                "gender": "",
+                "type": "",
+            }
+        });
+
+        axios.post('/patientSurvey', surveyData).then(data => {
+            this.props.history.push({
+                pathname: '/home',
+                surveyData: surveyData
+            })
+        }).catch(err => {
+            console.log('Error');
+        })
+    }
 
     render() {
         const { classes } = this.props;
-        console.log(this.props.location);
 
         return (
             <div className={classes.container}>
@@ -103,11 +59,7 @@ class patientSurvey extends Component {
                         <Typography variant='h4' style={{ paddingTop: '30%' }}>
                             Welcome Study for Patient
                         </Typography>
-                        <RadioGroupOne />
-                        <RadioGroupTwo />
-                        <Button variant="contained" color="primary">
-                            Submit
-                        </Button>
+                        <PatientForm callBackFromSurvey={this.handleSurvey} />
                     </Grid>
                     <Grid item xs={4}></Grid>
                 </Grid>
